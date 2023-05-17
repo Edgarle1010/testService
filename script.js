739522ee34dfd5fc16970c156d42a06058f8d737
Edgarle1010 <44484559+Edgarle1010@users.noreply.github.com>
2023-05-17 14:45:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Obtener el valor del parámetro llamado "parametro" de la URL
 var urlParams = new URLSearchParams(window.location.search);
-var parametro = urlParams.get('parametro');
+var parametro = urlParams.get('dato');
 
 // Asignar el valor del parámetro al elemento span
 var parametroValue = document.getElementById('parametroValue');
